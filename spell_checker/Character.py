c1332d54c22c6da2e45687e713fcb2bf1b0f3533
Igor Matheus Souza Moreira <imatheus.sm@gmail.com>
2019-05-28 16:44:51 -0300

    Methods reordered
    Yet another effort to make things readable.

--- spell_checker/Character.py
+++ spell_checker/Character.py
@@ -3,51 +3,6 @@ class Character:
 
 	Contains references to the sons (if any) and the father. The paths are the existing words.
 	"""
-	def __init__(self,character,previous_character = None,*args):
-		"""Class builder.
-
-		Parameters
-		----------
-		self : Character
-			A Character object.
-		character : str, int
-			The character to be incapsulated into a Character object. If an integer is provided, it will be automatically converted into a Character.
-		previous_character : Character (default = None)
-			The parent "Node". A TypeError Exception will be raised if an object of another type is provided.
-		*args : str, int
-			All remaining parameters eventually provided will be interpreted as next_characters (sons), will be incapsulated into Characters if needed and appended as sons of the current character.
-		Returns
-		-------
-		Character
-			A Character object.
-		"""
-		if isinstance(character,int) or isinstance(character,Character): character = str(character)
-		if not isinstance(character,str) or len(character) != 1: raise ValueError("A Character object can hold only one character.")
-		self.character = character.lower()
-		if previous_character != None and not isinstance(previous_character,Character): raise TypeError("The previous character must be a Character object as well.")
-		else: self.previous_character = previous_character
-		self.next_characters = sorted([Character(arg) for arg in args if isinstance(arg,str) and len(arg) == 1])
-		self.is_final = False
-
-	def __repr__(self):
-		"""Character representation.
-
-		Shows the character held by the Character class, as well as the previous one (parent) and next one (children).
-
-		Parameters
-		----------
-		self : Character
-			A Character object.
-
-		Returns
-		-------
-		str
-			The representation of the Character object, containing the aforementioned characteristics.
-		"""
-		return "<Character object>\n         Character: {}\nPrevious Character: {}\n   Next Characters: {}".format(self.character,
-																												   self.previous_character,
-																												   ", ".join([character.character for character in self.next_characters]))
-
 	def __eq__(self,other):
 		"""Checks if two Character objects contain the same characters (and previous and next as well).
 
@@ -100,6 +55,32 @@ class Character:
 		"""
 		return self.character > other.character
 
+	def __init__(self,character,previous_character = None,*args):
+		"""Class builder.
+
+		Parameters
+		----------
+		self : Character
+			A Character object.
+		character : str, int
+			The character to be incapsulated into a Character object. If an integer is provided, it will be automatically converted into a Character.
+		previous_character : Character (default = None)
+			The parent "Node". A TypeError Exception will be raised if an object of another type is provided.
+		*args : str, int
+			All remaining parameters eventually provided will be interpreted as next_characters (sons), will be incapsulated into Characters if needed and appended as sons of the current character.
+		Returns
+		-------
+		Character
+			A Character object.
+		"""
+		if isinstance(character,int) or isinstance(character,Character): character = str(character)
+		if not isinstance(character,str) or len(character) != 1: raise ValueError("A Character object can hold only one character.")
+		self.character = character.lower()
+		if previous_character != None and not isinstance(previous_character,Character): raise TypeError("The previous character must be a Character object as well.")
+		else: self.previous_character = previous_character
+		self.next_characters = sorted([Character(arg) for arg in args if isinstance(arg,str) and len(arg) == 1])
+		self.is_final = False
+
 	def __lt__(self,other):
 		"""Compares the characters incapsulated in the Character classes, disregarding the previous and next ones.
 
@@ -130,6 +111,26 @@ class Character:
 		True
 		"""
 		return self.character < other.character
+
+	def __repr__(self):
+		"""Character representation.
+
+		Shows the character held by the Character class, as well as the previous one (parent) and next one (children).
+
+		Parameters
+		----------
+		self : Character
+			A Character object.
+
+		Returns
+		-------
+		str
+			The representation of the Character object, containing the aforementioned characteristics.
+		"""
+		return "<Character object>\n         Character: {}\nPrevious Character: {}\n   Next Characters: {}".format(self.character,
+																												   self.previous_character,
+																												   ", ".join([character.character for character in self.next_characters]))
+
 
 	def __str__(self):
 		"""Converts the character into a string, omitting previous and next ones.
