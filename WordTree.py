82f1c2fefb9d77e76a52a74f3fbbfa04122a5e52
Igor Matheus Souza Moreira <imatheus.sm@gmail.com>
2019-05-28 12:10:54 -0300

    LetterTree.__init__() added (yes, it's called LetterTree now)
    In addition, some documentation was added, although in an extremely initial manner.
    
    Let's see how it develops. Before anything, though, let's rename the file to LetterTree first.

--- WordTree.py
+++ WordTree.py
@@ -2,3 +2,13 @@ from os import getcwd
 from sys import path as sys_path
 sys_path.append(getcwd())
 from Character import *
+
+class LetterTree:
+	"""Tree of letters, where each path, if final, represents a word.
+
+	Originally named WordTree, its name was changed because, in spite of representing words (or their non-existence), at the end of the day it comes down to a mere hierarchy of letters.
+	"""
+	def __init__(self,):
+		"""
+		"""
+		pass
