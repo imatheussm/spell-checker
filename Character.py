788bf7e4304107484f795310ae02536837d6aacd
Igor Matheus Souza Moreira <imatheus.sm@gmail.com>
2019-05-28 08:39:43 -0300

    Aesthetic changes
    No big deal, funtionally.

--- Character.py
+++ Character.py
@@ -1,11 +1,11 @@
 class Character:
     """Node of the dictionary tree.
-    
-    Will contain references to the sons (if any) and the father. The paths are the existing words.
+
+    Contains references to the sons (if any) and the father. The paths are the existing words.
     """
     def __init__(self,character,previous_character=None,*args):
         """Class builder.
-        
+
         Parameters
         ----------
         self : Character
@@ -21,18 +21,18 @@ class Character:
         Character
             A Character object.
         """
-        if isinstance(character,int) or isinstance(character,Character): character=str(character)
+        if isinstance(character,int) or isinstance(character,Character): character = str(character)
         if not isinstance(character,str) or len(character) != 1: raise ValueError("A Character object can hold only one character.")
         self.character = character
         if previous_character != None and not isinstance(previous_character,Character): raise TypeError("The previous character must be a Character object as well.")
         else: self.previous_character = previous_character
-        self.next_characters = sorted([Character(arg) for arg in args if isinstance(arg,str) and len(arg)==1])
-    
+        self.next_characters = sorted([Character(arg) for arg in args if isinstance(arg,str) and len(arg) == 1])
+
     def __repr__(self):
         """Character representation.
 
         Shows the character held by the Character class, as well as the previous one (parent) and next one (children).
-        
+
         Parameters
         ----------
         self : Character
@@ -49,9 +49,9 @@ class Character:
 
     def __eq__(self,other):
         """Checks if two Character objects contain the same characters (and previous and next as well).
-        
+
         In other words: checks if the three attributes (character, previous_character and next_characters) are the same.
-        
+
         Parameters
         ----------
         self : Character
@@ -130,12 +130,12 @@ class Character:
 
     def __str__(self):
         """Converts the character into a string, omitting previous and next ones.
-        
+
         Parameters
         ----------
         self : Character
             A Character object.
-        
+
         Returns
         -------
         str
