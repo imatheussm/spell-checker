b9cf5e8485a30ac2b7dcc580efdee35a0917643b
Igor Matheus Souza Moreira <imatheus.sm@gmail.com>
2019-05-29 10:03:53 -0300

    Another processing for added to from_csv()
    In hopes of mitigating processing time when loading up words, I created a low_memory=False option, which, instead of getting the words on an one-by-one basis through the words() generator(which got renamed to word_generator(), by the way). The results where quite frustrating, with the newer option even consuming slightly more time than the previous one (with low_memory=True). Anyway, I'll leave the new option here, at least for now.

--- spell_checker/__init__.py
+++ spell_checker/__init__.py
@@ -1,13 +1,15 @@
 from spell_checker.CharacterTree import *
-from random import random
+#from random import random
 
-def from_csv(data):
+def from_csv(data,low_memory=True):
 	"""Convert a CSV file into a CharacterTree object.
 
 	Parameters
 	----------
 	data : str
 		The path to a .csv file containing words to be loaded into the CharacterTree to be returned.
+	low_memory : bool (default = True)
+		In hopes of mitigating the load time without multi-threading nor other advanced stuff, this parameter was created. The idea was that if I loaded all words in memory instead of working with them one-at-a-time through the word_generator() generator, the processing time would decrease. In reality, though, after some testing the result was actually the opposite: when low_memory was set to True, the average processing time slightly increased.
 
 	Returns
 	-------
@@ -16,12 +18,18 @@ def from_csv(data):
 	"""
 	character_tree = CharacterTree()
 	with open (data,"r",encoding="utf-8") as file:
-		for word in words(file):
-			if random()<0.1: print(word)
-			character_tree.insert(word)
+		if low_memory == True:
+			for word in word_generator(file):
+				#if random()<0.1: print(word)
+				character_tree.insert(word)
+		else:
+			words = file.readlines()
+			words = [word.strip("\n") for word in words]
+			for word in words:
+				character_tree.insert(word)
 	return character_tree
 
-def words(data,case_sensitive=True):
+def word_generator(data,case_sensitive=True):
 	"""Word generator.
 
 	Parameters
