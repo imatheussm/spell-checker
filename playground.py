b1426ed9a60550141b37f433e80b278122a13c03
Igor Matheus Souza Moreira <imatheus.sm@gmail.com>
2019-05-29 10:05:18 -0300

    A lot of (commented out) testing code added
    These commented lines were used to test the time difference between the implementations I made of from_csv() (with low_memory=True and low_memory=False). I'll leve these lines here, at least for now.

--- playground.py
+++ playground.py
@@ -1,5 +1,6 @@
 from os import getcwd
 from sys import path as sys_path
+#from time import time
 sys_path.append(getcwd())
 from spell_checker import *
 
@@ -14,5 +15,35 @@ print("A palavra aba adicionada.")
 print("Há aba na árvore? Resposta: {}.".format("aba" in a))
 print("Perfeito.")
 
-ct = from_csv("D:\igor\OneDrive\Documentos\GitHub\spell-checker\palavras_sample.txt")
 print(", ".join([character.character for character in ct.next_characters]))
+
+#print("\n\n")
+
+
+#print("Comparing execution times:")
+
+#print("low_memory=True:",end="")
+#start_time = time()
+#ct = from_csv("D:\igor\OneDrive\Documentos\GitHub\spell-checker\palavras_sample.txt")
+#low_memory_time = time() - start_time
+#print(" {}".format(str(low_memory_time)))
+#
+#print("low_memory=False:",end="")
+#start_time = time()
+#ct = from_csv("D:\igor\OneDrive\Documentos\GitHub\spell-checker\palavras_sample.txt",False)
+#no_low_memory_time = time() - start_time
+#print(" {}".format(str(no_low_memory_time)))
+
+#print("low_memory=True:",end="")
+#start_time = time()
+#for i in range(5):
+#	print(i)
+#	ct = from_csv("D:\igor\OneDrive\Documentos\GitHub\spell-checker\palavras_sample.txt")
+#low_memory_time = (time() - start_time)/50
+
+#print("low_memory=False:",end="")
+#start_time = time()
+#for i in range(5):
+#	print(i)
+#	ct = from_csv("D:\igor\OneDrive\Documentos\GitHub\spell-checker\palavras_sample.txt",False)
+#no_low_memory_time = (time() - start_time)/50
