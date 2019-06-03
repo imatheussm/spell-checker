5ff4866e6cbb3c4b809d1ce6101baaf357faec05
Igor Matheus Souza Moreira <imatheus.sm@gmail.com>
2019-06-03 09:43:10 -0300

    Various changes to playground.py
    Boy, isnt't this a mess! I'll have to clean it up later, or just leave it as-is and work on the notebook instead. I don't know.

--- playground.py
+++ playground.py
@@ -1,8 +1,21 @@
+from random import random
 from os import getcwd
 from sys import path as sys_path
-#from time import time
+from time import time
 sys_path.append(getcwd())
 from spell_checker import *
+
+def sample_file(path,new_path,percentage=.20):
+	sample = []
+	with open(path,"r",encoding="utf-8") as file:
+		line = file.readline()
+		while line != "":
+			if random() < percentage: sample.append(line)
+			line = file.readline()
+
+	with open(new_path,"w",encoding="utf-8") as file:
+		for item in sample:
+			file.write(item)
 
 a = CharacterTree("abacate","mamão","maniçoba","queijo")
 print("Árvore criada com as palavras \"abacate\", \"mamão\",\"maniçoba\" e \"queijo\" nela.")
@@ -17,10 +30,21 @@ print("Perfeito.")
 
 print(", ".join([character.character for character in a.next_characters]))
 
-#print("\n\n")
+print("\n\n")
 
 
-#print("Comparing execution times:")
+print("Comparing execution times: ",end="")
+
+start_time = time()
+ct = from_csv("D:\igor\OneDrive\Documentos\GitHub\spell-checker\dictionaries\palavras.txt")
+low_memory_time = time() - start_time
+print("{}".format(str(low_memory_time)))
+
+#sample_file("D:\igor\OneDrive\Documentos\GitHub\spell-checker\dictionaries\palavras.txt",
+#			"D:\igor\OneDrive\Documentos\GitHub\spell-checker\dictionaries\palavras_sample1.txt")
+
+#wrong_words = ct.check("./texts/A-Caravana-de-Veneza.txt")
+wrong_words = ct.check("D:\igor\OneDrive\Documentos\GitHub\spell-checker\dictionaries\palavras_sample1.txt")
 
 #print("low_memory=True:",end="")
 #start_time = time()
