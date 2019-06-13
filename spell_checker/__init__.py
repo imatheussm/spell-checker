3ead2ba3fdc963d55b0e4100693087a12cf6a3c1
Igor Matheus Souza Moreira <imatheus.sm@gmail.com>
2019-06-13 17:55:13 -0300

    from_dict() adapted to support RadixTree objects
    Now, RadixTree objects are supported. Isn't that great?

--- spell_checker/__init__.py
+++ spell_checker/__init__.py
@@ -4,33 +4,38 @@ from string import punctuation
 from spell_checker.misc import *
 #from random import random
 
-def from_dict(data,character_tree=None,low_memory=True):
+def from_dict(data,type,tree=None,low_memory=True):
 	"""Convert files with one word per line into a CharacterTree object.
 
 	Parameters
 	----------
 	data : str (path)
-		The path to a file containing words to be loaded into the CharacterTree to be returned. There must be a word per line.
-	character_tree : bool (default = None)
-		The CharacterTree object in which the words are meant to be loaded. If not provided, will create a new CharacterTree object.
+		The path to a file containing words to be loaded into the CharacterTree or RadixTree to be returned. There must be a word per line.
+	tree : bool (default = None)
+		The CharacterTree or RadixTree object in which the words are meant to be loaded. If not provided, will create a new CharacterTree object.
 	low_memory : bool (default = True)
 		In hopes of mitigating the load time without multi-threading nor other advanced stuff, this parameter was created. The idea was that if I loaded all words in memory instead of working with them one-at-a-time through the word_generator() generator, the processing time would decrease. In reality, though, after some testing the result was actually the opposite: when low_memory was set to True, the average processing time slightly increased.
 
 	Returns
 	-------
-	CharacterTree
-		A CharacterTree object containing the loaded words provided.
+	CharacterTree, RadixTree
+		A CharacterTree or RadixTree object containing the loaded words provided.
 	"""
-	if character_tree == None: character_tree = CharacterTree()
-	elif not isinstance(character_tree,CharacterTree): raise TypeError("Parameter character_tree must be a CharacterTree object.")
+	try: type = type.lower()
+	except: raise TypeError("The type parameter must be a string.")
+	if type!="radix" and type!="character": raise ValueError("The available types are 'tree' and 'character'.")
+	if tree == None:
+		if type=="character": tree = CharacterTree()
+		else: tree = RadixTree()
+	elif not isinstance(tree,CharacterTree) and not isinstance(tree,RadixTree): raise TypeError("Parameter tree must be a CharacterTree or RadixTree object.")
 	with open (data,"r",encoding="utf-8") as file:
 		if low_memory == True:
 			for word in word_generator(file):
 				#if random()<0.1: print(word)
-				character_tree.insert(word)
+				tree.insert(word)
 		else:
-			for word in [word.strip("\n") for word in file.readlines()]: character_tree.insert(word)
-	return character_tree
+			for word in [word.strip("\n") for word in file.readlines()]: tree.insert(word)
+	return tree
 
 def from_txt(data,character_tree=None):
 	"""Loads words into a CharacterTree from the text file provided.
