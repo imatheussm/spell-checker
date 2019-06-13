1bbdec397003ba63c669f5fabf3dbc9d04891752
Igor Matheus Souza Moreira <imatheus.sm@gmail.com>
2019-06-13 17:58:28 -0300

    .__contains__() and .__repr__() implemented, .insert() improved
    The .__contains__() method now works, allowing us to see if a given word is in the Tree object or not. In addition, the .__repr__() method was implemented to properly show the necessary information, including the number of loaded words (which, so far, seems to match the one of the CharacterTree object). Finally, the .insert() method seems to be complete and stable, and got its debugging print statements commented out to make the loading process faster during testing. I'll probably add a verbose function to them later on; so far, however, let's leave them at that.

--- spell_checker/RadixTree.py
+++ spell_checker/RadixTree.py
@@ -6,7 +6,7 @@ class RadixTree(Radix):
 
 	Originally named WordTree (and then LetterTree), its name was changed because, in spite of representing words (or their non-existence), at the end of the day it comes down to a mere hierarchy of characters.
 	"""
-	def __contains__(self,other):
+	def __contains__(self,other,tree=None):
 		"""Checks if a word is contained in the CharacterTree.
 
 		In other words, checks if all characters are present in the object and if the last one is considered final.
@@ -24,7 +24,32 @@ class RadixTree(Radix):
 			The result of the verification.
 		"""
 		if isinstance(other,str):
-			raise NotImplementedError("To be implemented.")
+			#Finding the radix in the Tree among all the possibilities
+			if tree==None:
+				#print("No tree was defined. Defining tree to be self...")
+				tree = self
+			#else: print("tree = {} | word: {}".format(tree.radix,word))
+			if len(other) > 0:
+				if len(tree.next_radices)>0:
+					#print("There are children! Analyzing one-by-one...")
+					for next_radix in tree.next_radices:
+						#print("Current radix: {}".format(next_radix.radix))
+						try:
+							for i in range(len(next_radix.radix)):
+								if next_radix.radix[i] != other[i]:
+									continue
+								else:
+									if i == len(next_radix.radix)-1: return self.__contains__(other[i+1:],next_radix)
+						except IndexError:
+							continue
+					#print("None of the words are related! Appending...")
+					return False
+				else:
+					#print("There are no children! Appending...")
+					return False
+			else:
+				#print("Word processed!")
+				return tree.is_final
 		else:
 			return False
 
@@ -45,11 +70,27 @@ class RadixTree(Radix):
 		"""
 		super().__init__('*')
 		self.is_final = True
-		self.loaded_words = 0
+		self.loaded_words = 1
 		for arg in args:
 			if isinstance(arg,list) or isinstance(arg,tuple):
 				for item in arg: self.insert(item)
 			else: self.insert(arg)
+
+	def __repr__(self):
+		"""Representation of the RadixTree object.
+
+		Parameters
+		----------
+		self : RadixTree
+			A CharacterTree object.
+
+		Returns
+		-------
+		str
+			The string representation of the object.
+		"""
+		return "<RadixTree object>\n{} words loaded.\nAvailable Initial Characters: {}".format(self.loaded_words,
+																								   ", ".join([radix.radix for radix in self.next_radices]))
 
 	def find_radix(self,radix,tree):
 		"""Finds a radix and returns its index.
@@ -101,32 +142,32 @@ class RadixTree(Radix):
 			if str(i) in word: raise ValueError("Non-allowed characters were found in the word. Did your word contain any number or \"*\" as a character?")
 		#Finding the radix in the Tree among all the possibilities
 		if tree==None:
-			print("No tree was defined. Defining tree to be self...")
+			#print("No tree was defined. Defining tree to be self...")
 			tree = self
-		else: print("tree = {} | word: {}".format(tree.radix,word))
+		#else: print("tree = {} | word: {}".format(tree.radix,word))
 		if len(word) > 0:
 			if len(tree.next_radices)>0:
-				print("There are children! Analyzing one-by-one...")
+				#print("There are children! Analyzing one-by-one...")
 				for next_radix in tree.next_radices:
-					print("Current radix: {}".format(next_radix.radix))
+					#print("Current radix: {}".format(next_radix.radix))
 					try:
 						for i in range(len(next_radix.radix)):
 							if next_radix.radix[i] != word[i]:
-								print("{} is not equal to {}!".format(next_radix.radix[i],word[i]))
+								#print("{} is not equal to {}!".format(next_radix.radix[i],word[i]))
 								if i==0: break
 								else:
 									if len(word[i:])>0:
-										print("There's a relation, but there's a suffix! Calling .insert()...\n")
+										#print("There's a relation, but there's a suffix! Calling .insert()...\n")
 										new_next_radices = next_radix.next_radices
-										print("new_next_radices: {}".format(new_next_radices))
-										print("new Radix object:\n{}".format(Radix(next_radix.radix[i:],next_radix,new_next_radices).__repr__()))
+										#print("new_next_radices: {}".format(new_next_radices))
+										#print("new Radix object:\n{}".format(Radix(next_radix.radix[i:],next_radix,new_next_radices).__repr__()))
 										next_radix.next_radices = [Radix(next_radix.radix[i:],next_radix,new_next_radices)]
 										next_radix.radix = word[:i]
 										next_radix.is_final = False
 										del(new_next_radices)
 										return self.insert(word[i:],next_radix)
 									else:
-										if next_radix.is_final == True: raise Exception("The word {} has already been inserted!".format(word[:i]))
+										if next_radix.is_final == True: pass#print("The word {} has already been inserted!".format(word[:i]))
 										else:
 											next_radix.is_final = True
 											self.loaded_words += 1
@@ -136,15 +177,15 @@ class RadixTree(Radix):
 						if tree.is_final == False:
 							tree.is_final = True
 							self.loaded_words += 1
-				print("None of the words are related! Appending...")
+				#print("None of the words are related! Appending...")
 				tree.next_radices.append(Radix(word,tree))
 				self.loaded_words += 1
 			else:
-				print("There are no children! Appending...")
+				#print("There are no children! Appending...")
 				tree.next_radices.append(Radix(word,tree))
 				self.loaded_words += 1
 		else:
-			print("Word processed!")
+			#print("Word processed!")
 			if tree.is_final == False:
 				tree.is_final = True
 				self.loaded_words += 1
